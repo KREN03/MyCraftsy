b7d1f1a855f3659de80bc1d1aeef539b2d919705
andreaszico <zicoaritonang@gmail.com>
2021-11-08 15:31:51 +0700

    init bootstrap

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
     <title>Document</title>
 </head>
 <body>
     <h1>Home</h1>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+
+    <script src="{{ asset('js/bootstrap.js')  }}"></script>
 </body>
 </html>
