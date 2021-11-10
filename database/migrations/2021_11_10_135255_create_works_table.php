35801b239765b73ad6a8afcb3e68b1108aa37a14
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2021-11-10 21:28:32 +0700

    migration user category and works

---
+++ database/migrations/2021_11_10_135255_create_works_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->boolean('is_sell');
+            $table->integer('price');
+            $table->string('file');
+            $table->string('type');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
