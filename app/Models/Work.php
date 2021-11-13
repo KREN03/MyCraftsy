f36b83f472fe8a5bab8c3012cf525db29a12fbd1
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2021-11-13 21:28:17 +0700

    update model

--- app/Models/Work.php
+++ app/Models/Work.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Work extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'is_sell',
+        'price',
+        'file',
+        'type'
+    ];
 }
